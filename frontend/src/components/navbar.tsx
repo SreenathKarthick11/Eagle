452043d663178937e17cefdb35b7ddc2cc3f944b
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-04-10 20:05:30 +0530

    Frontend : Added a basic example navbar.

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,12 @@
+// Example component
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My App</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+      </ul>
+    </nav>
+  )
+}
