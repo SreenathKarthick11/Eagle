6383a28bcc70b9dc86637d418544f98445e66778
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-04-10 20:07:12 +0530

    Frontend : basic example app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react'
 import './App.css'
-
+import { Home } from './pages/Home';
 function App() {
   return (
     <div>
-      <p>Hello World</p>
+      <Home />
     </div>
   )
 }
