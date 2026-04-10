43466a0eb2f5679fb98335a630fb5f7c64617435
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-04-10 20:06:25 +0530

    Frontend : Added a basic example home page.

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// Home Page
+
+import { Navbar } from '../components/navbar';
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  )
+}
