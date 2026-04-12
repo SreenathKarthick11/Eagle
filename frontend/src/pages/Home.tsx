5c0a39bd4af74f5f2128ee77f302ae0e40adbf5c
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-04-12 19:36:30 +0530

    Frontend : Fixed Navbar bug in Home page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 // Home page
 import { Navbar } from '../components/navbar';
-
+import './styles/Home.css';
 export const Home = () => {
 
   return (
     <div>
       <Navbar />
-      <h2>Welcome to the Home Page</h2>      
+      <div className='content'>
+        <h2>Welcome to the Home Page</h2>  
+      </div>
     </div>
   );
 };
