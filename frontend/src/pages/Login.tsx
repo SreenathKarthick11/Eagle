b20d8d9def9b98eef878a060ec2959b75cace809
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-04-12 16:26:34 +0530

    Frontend : Completed Design of Login Page.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import "./styles/Login.css";
 
+import { useNavigate } from "react-router-dom";
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/button/filled-button.js';
 
@@ -8,11 +9,14 @@ export const Login = () => {
   const emailRef = useRef<any>(null);
   const passwordRef = useRef<any>(null);
 
+  const navigate = useNavigate();
+
   const handleLogin = () => {
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
 
     console.log(email, password);
+    navigate("/");
   };
 
   return (
@@ -26,8 +30,12 @@ export const Login = () => {
         {/* @ts-ignore */}
         <md-outlined-text-field ref={passwordRef} label="Password" type="password" class="login-input" ></md-outlined-text-field>
 
-        {/* @ts-ignore */}
-        <md-filled-button class="login-button" onClick={handleLogin}> Login </md-filled-button>
+        <div className="btn-container">
+          {/* @ts-ignore */}
+          <md-filled-button class="button" onClick={handleLogin}> Login </md-filled-button>
+          {/* @ts-ignore */}
+          <md-filled-button class="button" onClick={()=> navigate("/signup") }> SignUp </md-filled-button>
+        </div>
       </div>
     </div>
   );
