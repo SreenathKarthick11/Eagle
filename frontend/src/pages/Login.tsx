aaeb50d246d56ba7bd1f489e2e66fcecb24d579c
Raagam Parmar <raagamparmar@gmail.com>
2026-04-30 02:16:28 +0530

    Frontend: Updated login UI to be much closer to Material UI.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,17 @@
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import type { MdOutlinedTextField } from "@material/web/textfield/outlined-text-field.js";
+
+import "@material/web/textfield/outlined-text-field.js";
+import "@material/web/button/filled-button.js";
+import "@material/web/button/outlined-button.js";
+
 import "./styles/Login.css";
 
-import { useNavigate } from "react-router-dom";
-import '@material/web/textfield/outlined-text-field.js';
-import '@material/web/button/filled-button.js';
-
 export const Login = () => {
-  const usernameRef = useRef<any>(null);
-  const passwordRef = useRef<any>(null);
+  const usernameRef = useRef<MdOutlinedTextField>(null);
+  const passwordRef = useRef<MdOutlinedTextField>(null);
 
   const navigate = useNavigate();
 
@@ -15,26 +19,41 @@ export const Login = () => {
     const username = usernameRef.current?.value;
     const password = passwordRef.current?.value;
 
+    if (!username || !password) {
+      alert("Please fill in all fields");
+      return;
+    }
+
     console.log(username, password);
     navigate("/");
   };
 
   return (
     <div className="login-container">
-      <div className="login-card">
+      <div className="login-grid">
         <h2 className="login-title">Login</h2>
 
-        {/* @ts-ignore */}
-        <md-outlined-text-field ref={usernameRef} label="Username" class="login-input" ></md-outlined-text-field>
+        <hr className="divider" />
 
-        {/* @ts-ignore */}
-        <md-outlined-text-field ref={passwordRef} label="Password" type="password" class="login-input" ></md-outlined-text-field>
+        <md-outlined-text-field
+          ref={usernameRef}
+          label="Username"
+          className="login-input"
+        ></md-outlined-text-field>
 
-        <div className="btn-container">
-          {/* @ts-ignore */}
-          <md-filled-button class="button" onClick={handleLogin}> Login </md-filled-button>
-          {/* @ts-ignore */}
-          <md-filled-button class="button" onClick={()=> navigate("/signup") }> SignUp </md-filled-button>
+        <md-outlined-text-field
+          ref={passwordRef}
+          label="Password"
+          type="password"
+          className="login-input"
+        ></md-outlined-text-field>
+
+        <div className="btn-group">
+
+          <md-outlined-button onClick={() => navigate("/signup")}>
+            Sign Up
+          </md-outlined-button>
+          <md-filled-button onClick={handleLogin}>Login</md-filled-button>
         </div>
       </div>
     </div>
