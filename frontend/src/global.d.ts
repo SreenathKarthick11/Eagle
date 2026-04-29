a87830a5b891e13c72ea9c12e78018a224aca0dc
Raagam Parmar <raagamparmar@gmail.com>
2026-04-29 23:13:19 +0530

    Frontend: Updated global declaration file.

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,11 +1,110 @@
-import 'react';
+import "react";
 
-declare module 'react' {
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      'md-text-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      'md-list': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      'md-list-item': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      /* Buttons */
+      // Elevated Buttons
+      "md-elevated-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Filled Buttons
+      "md-filled-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Filled Tonal Buttons
+      "md-filled-tonal-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Outlined Buttons
+      "md-outlined-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Text Buttons
+      "md-text-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+
+
+      /* Lists */
+      // List Body
+      "md-list": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // List Item
+      "md-list-item": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+
+
+      /* Select */
+      // Outlined Select
+      "md-outlined-select": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Filled Select
+      "md-filled-select": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Select Option
+      "md-select-option": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+
+
+      /* Dialogs */
+      "md-dialog": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & { open?: boolean };
+
+
+
+      /* Icons */
+      "md-icon": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & { filled?: boolean };
+
+
+      /* Icon Buttons */
+      // Icon Buttons
+      "md-icon-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Filed Icon Button
+      "md-filled-icon-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+
+      // Filled Tonal Icon Button
+      "md-filled-tonal-icon-button": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
     }
   }
 }
