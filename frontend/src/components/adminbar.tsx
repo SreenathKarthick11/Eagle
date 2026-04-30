94f5aedc9a0bb87f2e450e396eeb839b931df1a7
Raagam Parmar <raagamparmar@gmail.com>
2026-04-30 16:33:13 +0530

    Frontend: Modified adminbar for vertical navigation.

--- frontend/src/components/adminbar.tsx
+++ frontend/src/components/adminbar.tsx
@@ -6,32 +6,30 @@ export const Adminbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="adminbar">
-      <div className="nav-buttons">
-        <md-text-button onClick={() => navigate("/admin/blacklist")}>
-          Blacklist
-        </md-text-button>
+    <div className="sidebar">
+      <md-text-button onClick={() => navigate("/admin/blacklist")}>
+        Blacklist
+      </md-text-button>
 
-        <md-text-button onClick={() => navigate("/admin/campuses")}>
-          Campuses
-        </md-text-button>
+      <md-text-button onClick={() => navigate("/admin/campuses")}>
+        Campuses
+      </md-text-button>
 
-        <md-text-button onClick={() => navigate("/admin/locations")}>
-          Locations
-        </md-text-button>
+      <md-text-button onClick={() => navigate("/admin/locations")}>
+        Locations
+      </md-text-button>
 
-        <md-text-button onClick={() => navigate("/admin/venues")}>
-          Venues
-        </md-text-button>
+      <md-text-button onClick={() => navigate("/admin/venues")}>
+        Venues
+      </md-text-button>
 
-        <md-text-button onClick={() => navigate("/admin/events")}>
-          Events
-        </md-text-button>
+      <md-text-button onClick={() => navigate("/admin/events")}>
+        Events
+      </md-text-button>
 
-        <md-text-button onClick={() => navigate("/admin/promote")}>
-          Users
-        </md-text-button>
-      </div>
-    </nav>
+      <md-text-button onClick={() => navigate("/admin/promote")}>
+        Users
+      </md-text-button>
+    </div>
   );
 };
