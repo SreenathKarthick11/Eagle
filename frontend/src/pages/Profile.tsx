684e3841ce5766f24869e4b2ae7b912edc924d54
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-05-01 22:38:37 +0530

    Frontend: Updated dialog boxes for profile page.

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Navbar } from "../components/navbar";
+import { useEffect, useRef, useState } from "react";
 import "./styles/Profile.css";
 
 import '@material/web/textfield/outlined-text-field.js';
@@ -7,7 +6,13 @@ import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/dialog/dialog.js';
 
+import { MdDialog } from "@material/web/dialog/dialog.js";
+import type { MdOutlinedTextField } from "@material/web/textfield/outlined-text-field.js";
+
 export const Profile = () => {
+  const dialogSaveChangesRef = useRef<MdDialog>(null);
+  const dialogChangePassRef = useRef<MdDialog>(null);
+
   const [userData, setUserData] = useState({
     name: "Sreenath",
     username: "sreenath_admin",
@@ -15,34 +20,48 @@ export const Profile = () => {
     phone: "1234567890",
   });
 
-  const [isPasswordOpen, setIsPasswordOpen] = useState(false);
-  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
-
   const handleSave = () => {
-    // Logic for updating profile
-    setIsSuccessOpen(true);
+    console.log("Changes saved.");
+    dialogSaveChangesRef.current?.show();
   };
 
   const handlePasswordChange = () => {
-    // Logic for changing password
-    setIsPasswordOpen(false);
-    setIsSuccessOpen(true);
+    console.log("Password changed.");
+    dialogChangePassRef.current?.close();
+    dialogSaveChangesRef.current?.show();
   }
 
+  const handlePasswordCancel = () => {
+    console.log("Password change cancelled.");
+    dialogChangePassRef.current?.close();
+  }
+
+  const handlePasswordChangeOpen = () => {
+    dialogChangePassRef.current?.show();
+  }
+
+  useEffect(() => {
+    const dialogSaveChanges = dialogSaveChangesRef.current;
+    const dialogChangePass  = dialogChangePassRef.current;
+    if (!dialogSaveChanges) return;
+    if (!dialogChangePass) return;
+  }, []);
+
   return (
-    <div className="ProfilePage">      
+    <div className="ProfilePage">
       <div className="content profile-page">
         <h2 className="page-header">User Profile</h2>
-        
+
         <div className="profile-form">
-          {/* @ts-ignore */}
           <md-outlined-text-field
             label="Name"
             value={userData.name}
-            onInput={(e) => setUserData({...userData, name: e.target.value})}
+            onInput={(e: React.InputEvent<MdOutlinedTextField>) => {
+              const target = e.target as MdOutlinedTextField;
+              setUserData({ ...userData, name: target.value });
+            }}
           />
 
-          {/* @ts-ignore */}
           <md-outlined-text-field
             label="Username"
             value={userData.username}
@@ -50,7 +69,6 @@ export const Profile = () => {
             disabled
           />
 
-          {/* @ts-ignore */}
           <md-outlined-text-field
             label="Email"
             value={userData.email}
@@ -58,58 +76,44 @@ export const Profile = () => {
             disabled
           />
 
-          {/* @ts-ignore */}
           <md-outlined-text-field
             label="Phone"
+            type="number"
             value={userData.phone}
-            onInput={(e) => setUserData({...userData, phone: e.target.value})}
+            onInput={(e: React.InputEvent<MdOutlinedTextField>) => {
+              const target = e.target as MdOutlinedTextField;
+              setUserData({ ...userData, phone: target.value });
+            }}
           />
 
           <div className="profile-footer">
-            {/* @ts-ignore */}
-            <md-outlined-button onClick={() => setIsPasswordOpen(true)}> Change Password</md-outlined-button>
-            
-            {/* @ts-ignore */}
+            <md-outlined-button onClick={handlePasswordChangeOpen}> Change Password</md-outlined-button>
+
             <md-filled-button onClick={handleSave}>Save Changes</md-filled-button>
           </div>
         </div>
       </div>
 
       {/* CHANGE PASSWORD DIALOG */}
-      {/* @ts-ignore */}
-      <md-dialog 
-        open={isPasswordOpen} 
-        onClosed={() => setIsPasswordOpen(false)}>
+      <md-dialog ref={dialogChangePassRef}>
         <div slot="headline">Change Password</div>
         <form slot="content" id="pw-form" className="dialog-form">
-          {/* @ts-ignore */}
           <md-outlined-text-field label="Current Password" type="password" />
-          {/* @ts-ignore */}
           <md-outlined-text-field label="New Password" type="password" />
-          {/* @ts-ignore */}
           <md-outlined-text-field label="Confirm New Password" type="password" />
         </form>
         <div slot="actions">
-          {/* @ts-ignore */}
-          <md-text-button onClick={() => setIsPasswordOpen(false)}>Cancel</md-text-button>
-          {/* @ts-ignore */}
+          <md-text-button onClick={handlePasswordCancel}>Cancel</md-text-button>
           <md-filled-button onClick={handlePasswordChange}>Update</md-filled-button>
         </div>
-      {/* @ts-ignore */}
       </md-dialog>
 
       {/* SUCCESS DIALOG */}
-      {/* @ts-ignore */}
-      <md-dialog 
-        open={isSuccessOpen} 
-        onClosed={() => setIsSuccessOpen(false)}>
-        <div slot="headline">Success</div>
-        <div slot="content">Your profile has been updated successfully.</div>
+      <md-dialog ref={dialogSaveChangesRef}>
+        <div slot="headline">Successfully updated!</div>
         <div slot="actions">
-          {/* @ts-ignore */}
-          <md-text-button onClick={() => setIsSuccessOpen(false)}>Close</md-text-button>
+          <md-text-button onClick={() => dialogSaveChangesRef.current?.close()}>Ok</md-text-button>
         </div>
-        {/*@ts-ignore*/}
       </md-dialog>
     </div>
   );
