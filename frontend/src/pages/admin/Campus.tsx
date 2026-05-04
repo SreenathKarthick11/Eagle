75b22f6614a16144b8d085564f9fc342f7ec624b
Raagam Parmar <raagamparmar@gmail.com>
2026-05-05 00:20:32 +0530

    Frontend: Added temporary API for admin campus management.

--- frontend/src/pages/admin/Campus.tsx
+++ frontend/src/pages/admin/Campus.tsx
@@ -10,54 +10,89 @@ import "@material/web/icon/icon.js";
 
 import type { MdDialog } from "@material/web/dialog/dialog.js";
 import type { MdOutlinedTextField } from "@material/web/textfield/outlined-text-field.js";
+import type { CampusItem } from "../../interfaces";
+import type { DialogHandle } from "../../components/customDialog";
 
 import "../styles/admin/Campus.css";
 
 export const AdminCampus = () => {
-  const [selectedCampus, setSelectedCampus] = useState<string | null>(null);
-  const [campus, setCampus] = useState<string>("");
+  const [selectedCampus, setSelectedCampus] = useState<CampusItem | null>(null);
+  const [campusList, setCampusList] = useState<CampusItem[]>([]);
+  const [newCampus, setNewCampus] = useState<string>("");
 
   const dialogAddRef = useRef<MdDialog>(null);
   const dialogRemoveRef = useRef<MdDialog>(null);
+  const fetchErrorDialogReg = useRef<DialogHandle>(null);
 
-  const campuses = ["Nila", "Sahyadri", "Ahalia"];
+  const showError = (msg: string) => {
+    fetchErrorDialogReg.current?.open("Error", msg);
+  };
 
   useEffect(() => {
     const dialog = dialogAddRef.current;
     if (!dialog) return;
-
-    const handleOpened = () => console.log("Dialog animation finished: Open");
-    const handleClosed = () => {
-      console.log("Dialog animation finished: Closed");
-    };
-
-    dialog.addEventListener("opened", handleOpened);
-    dialog.addEventListener("closed", handleClosed);
-
-    return () => {
-      dialog.removeEventListener("opened", handleOpened);
-      dialog.removeEventListener("closed", handleClosed);
-    };
   }, []);
 
+  useEffect(() => {
+    const loadCampuses = async () => {
+      try {
+        const res = await fetch("https://localhost:8000/campuses/");
+        const data: CampusItem[] = await res.json();
+        setCampusList(data);
+      } catch {
+        showError("Server Error");
+      }
+    };
+    loadCampuses();
+  });
+
   const openConfirmAddDialog = (campus: string) => {
-    setSelectedCampus(campus);
+    if (campus === "") return;
+    setNewCampus(campus);
     dialogAddRef.current?.show();
   };
 
   const confirmAdd = () => {
-    console.log("Added campus:", selectedCampus);
+    console.log("Added campus:", newCampus);
     dialogAddRef.current?.close();
   };
 
-  const openConfirmRemoveDialog = (campus: string) => {
+  const openConfirmRemoveDialog = (campus: CampusItem) => {
     setSelectedCampus(campus);
     dialogRemoveRef.current?.show();
   };
 
-  const confirmRemove = () => {
-    console.log("Removed campus:", selectedCampus);
-    dialogRemoveRef.current?.close();
+  const confirmRemove = async () => {
+    if (!selectedCampus) return;
+
+    try {
+      const res = await fetch("https://localhost:8000/api/delete_campus", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          campud_id: selectedCampus.campud_id,
+        }),
+      });
+
+      let data: { detail?: string } = {};
+      data = await res.json();
+
+      if (!res.ok) {
+        showError(data.detail || "Failed to add campus");
+        return;
+      }
+
+      setCampusList((prev) =>
+        prev.filter((v) => v.campud_id !== selectedCampus.campud_id),
+      );
+
+      console.log("Removed campus:", selectedCampus?.campus_name);
+      dialogRemoveRef.current?.close();
+    } catch {
+      showError("Server Error");
+    }
   };
 
   return (
@@ -66,23 +101,23 @@ export const AdminCampus = () => {
 
       <md-filled-text-field
         label="Name"
-        value={campus}
+        value={newCampus}
         onInput={(e: React.InputEvent<MdOutlinedTextField>) => {
           const target = e.target as MdOutlinedTextField;
-          setCampus(target.value);
+          setNewCampus(target.value);
         }}
       />
 
-      <md-filled-button onClick={() => openConfirmAddDialog(campus)}>
+      <md-filled-button onClick={() => openConfirmAddDialog(newCampus)}>
         Add
       </md-filled-button>
 
       <h1>Remove Campus</h1>
 
       <md-list className="campus_list">
-        {campuses.map((campus, index) => (
+        {campusList.map((campus, index) => (
           <md-list-item key={index}>
-            <div slot="headline">{campus}</div>
+            <div slot="headline">{campus.campus_name}</div>
             <md-filled-tonal-icon-button
               slot="end"
               onClick={() => openConfirmRemoveDialog(campus)}
@@ -95,7 +130,7 @@ export const AdminCampus = () => {
 
       <md-dialog ref={dialogRemoveRef}>
         <div slot="headline">
-          Remove campus <b>{selectedCampus}?</b>
+          Remove campus <b>{selectedCampus?.campus_name}?</b>
         </div>
         <div slot="actions">
           <md-text-button onClick={() => dialogRemoveRef.current?.close()}>
@@ -107,7 +142,7 @@ export const AdminCampus = () => {
 
       <md-dialog ref={dialogAddRef}>
         <div slot="headline">
-          Add campus <b>{campus}?</b>
+          Add campus <b>{newCampus}?</b>
         </div>
         <div slot="actions">
           <md-text-button onClick={() => dialogAddRef.current?.close()}>
